54f22efd5d32779a65f5e6916595e90bba1359b2
suyong <edgarlee9364@gmail.com>
2024-08-01 16:05:21 +0900

    feat: 프로그래머스 Level 0 인덱스 바꾸기

---
+++ Programmers/알고리즘 입문/인덱스 바꾸기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int num1, int num2) {
+    string answer = "";
+
+    char temp = my_string[num1];
+
+    my_string[num1] = my_string[num2];
+
+    my_string[num2] = temp;
+
+    answer = my_string;
+
+    return answer;
+}
