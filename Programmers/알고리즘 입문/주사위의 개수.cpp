2adc28d5c8de5e45eb37523151ab1885ec72c13b
suyong <edgarlee9364@gmail.com>
2024-08-01 16:05:35 +0900

    feat: 프로그래머스 Level 0 주사위의 개수

---
+++ Programmers/알고리즘 입문/주사위의 개수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> box, int n) {
+    int answer = 0;
+
+    int a = box[0] / n;
+    int b = box[1] / n;
+    int c = box[2] / n;
+
+    int d = a * b * c;
+
+    answer = d;
+
+    return answer;
+}
