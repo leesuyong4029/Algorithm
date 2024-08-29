172a82abeaf643b9b3c4a6ba95b39d56386ddaa9
suyong <edgarlee9364@gmail.com>
2024-08-29 14:31:40 +0900

    feat: 프로그래머스 Level 0 로그인 성공?

---
+++ Programmers/알고리즘 입문/로그인 성공.py
@@ -0,0 +1,10 @@
+def solution(id_pw, db):
+    db_dict = {i[0]: i[1] for i in db}
+    if id_pw[0] in db_dict:
+        if id_pw[1] == db_dict[id_pw[0]]:
+            return "login"
+        else:
+            return "wrong pw"
+
+    else:
+        return "fail"
